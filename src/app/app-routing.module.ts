ab2191d2abf7e85306078ffe4f5843af900159d7
Mouhamed <diallo.mouhamed288@gmail.com>
2022-04-25 15:04:22 +0000

    CRUD Eleves et Liste Classe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AfficherElevesComponent } from './afficherEleves/afficherEleves.component';
+import { ClasseComponent } from './classe/classe.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  //{ path: '', redirectTo: 'afficherEleves', pathMatch: 'full'},
+  { path: '', component: ClasseComponent },
+  { path: 'afficherEleves', component: AfficherElevesComponent, }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AfficherElevesComponent } from './afficherEleves/afficherEleves.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClasseComponent } from './classe/classe.component';
+
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AfficherElevesComponent,
+    ClasseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
